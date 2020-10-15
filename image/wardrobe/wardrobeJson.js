1de2a76ec595be44ebea02f13d45dd6c4bcc0d74
dongrui <dongrui@ehousechina.com>
2020-10-15 10:01:40 +0800

    数据

---
+++ image/wardrobe/wardrobeJson.js
@@ -0,0 +1,74 @@
+let types = [
+    {
+      value:0,
+      label:'duanxiu',
+      title:'短袖',
+      icon:'icon-duanxiu',
+      length:1//数量
+    },
+    {
+      value:1,
+      label:'chenshan',
+      title:'衬衫',
+      icon:'icon-xizhuangwaitao',
+      length:8
+    },
+    {
+      value:2,
+      label:'waitao',
+      title:'外套',
+      icon:'icon-waitao',
+      length:1
+    },
+    {
+      value:4,
+      label:'kuzi',
+      title:'裤子',
+      icon:'icon-kuzi',
+      length:1
+    },
+    {
+      value:2,
+      label:'qunzi',
+      title:'裙子',
+      icon:'icon-qunzi',
+      length:1
+    },
+    {
+      value:3,
+      label:'xiezi',
+      title:'鞋子',
+      icon:'icon-xiezi',
+      length:1
+    },
+    {
+      value:4,
+      label:'baobao',
+      title:'包包',
+      icon:'icon-baobao',
+      length:1
+    },
+    {
+      value:4,
+      label:'peishi',
+      title:'配饰',
+      icon:'icon-peishi',
+      length:1
+    },
+    {
+      value:5,
+      label:'shipin',
+      title:'饰品',
+      icon:'icon-shipin',
+      length:1
+    }
+    // {
+    //   value:5,
+    //   label:'chuanda',
+    //   title:'穿搭',
+    //   icon:'icon-ziyuan',
+    //   length:1,
+    // },
+  ]
+
+export default types;
